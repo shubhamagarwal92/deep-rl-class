ba87d6218a29f2d933d28475efdd1ba705742cdd
Shubham Agarwal <shubhamagarwal92@gmail.com>
2023-08-08 00:43:16 -0400

    setup instructions

--- notebooks/unit7/setup.sh
+++ notebooks/unit7/setup.sh
@@ -1,13 +1,15 @@
+# https://huggingface.co/learn/deep-rl-course/unit7/hands-on
 conda create --name rl python=3.9 -y
 conda activate rl
 
 git clone https://github.com/Unity-Technologies/ml-agents
 
 cd ml-agents
+# TODO: SA
 pip install -e ./ml-agents-envs
 pip install -e ./ml-agents
 
-pip install torch
+#pip install torch
 
 pip install gdown
 
@@ -20,6 +22,10 @@ git lfs install
 
 # Config in ./config/poca/SoccerTwos.yaml
 
-wget https://drive.google.com/file/d/1KuqBKYiXiIcU4kNMqEzhgypuFP5_45CL/view?usp=sharing
-
+#wget https://drive.google.com/file/d/1KuqBKYiXiIcU4kNMqEzhgypuFP5_45CL/view?usp=sharing
 gdown 1KuqBKYiXiIcU4kNMqEzhgypuFP5_45CL
+
+mkdir -p ml-agents/training-envs-executables
+# If not in repo dir --> ml-agents/ml-agents
+mv SoccerTwos.zip ml-agents/ml-agents/training-envs-executables/
+unzip SoccerTwos.zip
