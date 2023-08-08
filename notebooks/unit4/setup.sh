7e36e85431ed2a28dbd3dbb55ebb056daab3ef5d
Shubham Agarwal <shubhamagarwal92@gmail.com>
2023-08-08 00:02:40 -0400

    setup sh

--- notebooks/unit4/setup.sh
+++ notebooks/unit4/setup.sh
@@ -9,3 +9,8 @@ conda activate rl
 pip install pyvirtualdisplay
 pip install pyglet==1.5.1
 pip install -r https://raw.githubusercontent.com/huggingface/deep-rl-class/main/notebooks/unit4/requirements-unit4.txt
+
+pip install matplotlib
+pip install torch
+
+# conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
